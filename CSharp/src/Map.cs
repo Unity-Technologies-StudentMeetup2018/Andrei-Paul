5d3ad31cea9fcd59204bca716dff87b5405270fa
Ghitea Andrei Paul <ghitea.andrei.paul@gmail.com>
2018-04-19 20:23:57 +0200

    Added... Everything

--- CSharp/src/Map.cs
+++ CSharp/src/Map.cs
@@ -23,6 +23,15 @@ namespace PathfindingCSharp
         Vector2 startPosition;
         Vector2 goalPosition;
 
+        char[] markerFace = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+        int[,] mapWork;
+        double theta;
+
+        int[] OffsetX = { -1, -1, -1, 0, 0, 1, 1, 1 };
+        int[] OffsetY = { -1, 0, 1, -1, 1, -1, 0, 1 };
+        double[] OffsetAngle = { 0, 0, 0, 0, 0, 0, 0, 0 };
+        int OffsetSize = 8;
+
         public Map(string[] mapData, Vector2 startPos, Vector2 endPos)
         {
             if (startPos.x < 0 || startPos.x >= mapData.Length || startPos.y < 0 || startPos.y >= mapData.Length)
@@ -101,10 +110,126 @@ namespace PathfindingCSharp
             return sb.ToString();
         }
 
+        public void fartInGeneralDirection()
+        {
+            for (int Index = 0; Index < OffsetSize; Index++)
+            {
+                double slope = (double)(OffsetY[Index]) / (double)(OffsetX[Index]);
+                OffsetAngle[Index] = Math.Atan(slope);
+            }
+        }
+
+        public int[,,] fartInParticularDirection(int x, int y)
+        {
+            double slope = (double)(goalPosition.y - y) / (double)(goalPosition.x - x);
+            double theta = Math.Atan(slope);
+
+            int StartOffset = 0;
+            for (int Index = 0; Index < OffsetSize; Index++)
+            {
+//                if(OffsetAngle - )
+            }
+
+            return null;
+        }
+
+        public bool lazyOmniWalk(int x, int y)
+        {
+            int marker = mapWork[y, x] + 1;
+
+            mapWork[y, x] = -mapWork[y, x];
+            if (x == goalPosition.x && y == goalPosition.y)
+            {
+                Console.WriteLine("\nAfter 'only' " + marker + " steps!\n");
+                return true;
+            }
+
+            fartInParticularDirection(x, y);
+            for (int Index = 0; Index < OffsetSize; Index++)
+            {
+                int X = x + OffsetX[Index];
+                int Y = y + OffsetY[Index];
+                if (X < 0 || Y < 0 || X > 31 || Y > 31)
+                {
+                    continue;
+                }
+
+                if (mapWork[Y, X] == 0)
+                {
+                    mapWork[Y, X] = marker;
+                }
+            }
+
+            for (int Index = 0; Index < OffsetSize; Index++)
+            {
+                int X = x + OffsetX[Index];
+                int Y = y + OffsetY[Index];
+                if (X < 0 || Y < 0 || X > 31 || Y > 31)
+                {
+                    continue;
+                }
+
+                if (mapWork[Y, X] == marker)
+                {
+                    bool stop = lazyOmniWalk(X, Y);
+
+                    if (stop == true)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        mapWork[Y, X] = int.MinValue;
+                    }
+                }
+            }
+
+            return false;
+        }
+
         public bool ComputePath()
         {
-            //TODO: Implement solution here
-            return false;
+            fartInGeneralDirection();
+            mapWork = new int[32, 32];
+
+            for (int X = 0; X < 32; X++)
+            {
+                for (int Y = 0; Y < 32; Y++)
+                {
+                    if (mapPositions[Y, X] == '#')
+                    {
+                        mapWork[Y, X] = int.MaxValue;
+                    }
+                    else
+                    {
+                        mapWork[Y, X] = 0;
+                    }
+                }
+            }
+
+            mapWork[startPosition.y, startPosition.x] = 1;
+            bool result = lazyOmniWalk(startPosition.x, startPosition.y);
+
+            for (int X = 0; X < 32; X++)
+            {
+                for (int Y = 0; Y < 32; Y++)
+                {
+                    if (mapWork[Y, X] != int.MaxValue && mapWork[Y, X] != int.MinValue && mapPositions[Y, X] != '@' && mapWork[Y, X] != 0)
+                    {
+                        if (mapWork[Y, X] < 0)
+                        {
+                            mapWork[Y, X] = -mapWork[Y, X];
+                        }
+
+                        int markerIndex = mapWork[Y, X] % markerFace.Length;
+                        mapPositions[Y, X] = ' ';// markerFace[markerIndex];
+                    }
+                }
+            }
+
+            DisplayMap();
+
+            return result;
         }
     };
 }
